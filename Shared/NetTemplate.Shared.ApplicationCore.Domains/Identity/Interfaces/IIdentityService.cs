d7c67c75662f455280c736d919974c04ae5eee65
trannamtrung1st <trannamtrung1st@gmail.com>
2023-06-02 12:36:20 +0700

    Remove domain services

--- Shared/NetTemplate.Shared.ApplicationCore.Domains/Identity/Interfaces/IIdentityService.cs
+++ Shared/NetTemplate.Shared.ApplicationCore.Domains/Identity/Interfaces/IIdentityService.cs
@@ -1,9 +1,8 @@
-﻿using NetTemplate.Shared.ApplicationCore.Common.Interfaces;
-using NetTemplate.Shared.ApplicationCore.Domains.Identity.Models;
+﻿using NetTemplate.Shared.ApplicationCore.Domains.Identity.Models;
 
 namespace NetTemplate.Shared.ApplicationCore.Domains.Identity.Interfaces
 {
-    public interface IIdentityService : IDomainService
+    public interface IIdentityService
     {
         Task<IEnumerable<IdentityUserModel>> GetIdentityUsers(CancellationToken cancellationToken = default);
     }
