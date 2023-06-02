b5db0b4dd4368b876aa3b36779b098f695220817
trannamtrung1st <trannamtrung1st@gmail.com>
2023-06-02 12:32:29 +0700

    Refactor result codes

--- Shared/NetTemplate.Shared.ApplicationCore/Common/ResultCodes.cs
+++ Shared/NetTemplate.Shared.ApplicationCore/Common/ResultCodes.cs
@@ -10,14 +10,14 @@ namespace NetTemplate.Shared.ApplicationCore.Common
             private const int Base = 0;
             private const string Group = nameof(Common);
 
-            public static ResultCode ObjectResult = new ResultCode(Base + 1, CommonMessages.ObjectResult, Group);
-            public static ResultCode NotFound = new ResultCode(Base + 2, CommonMessages.NotFound, Group);
-            public static ResultCode BadRequest = new ResultCode(Base + 3, CommonMessages.BadRequest, Group);
-            public static ResultCode AccessDenied = new ResultCode(Base + 4, CommonMessages.AccessDenied, Group);
-            public static ResultCode UnknownError = new ResultCode(Base + 5, CommonMessages.UnknownError, Group);
-            public static ResultCode PersistenceError = new ResultCode(Base + 6, CommonMessages.PersistenceError, Group);
-            public static ResultCode InvalidPagination = new ResultCode(Base + 7, CommonMessages.InvalidPagination, Group);
-            public static ResultCode InvalidData = new ResultCode(Base + 8, CommonMessages.InvalidData, Group);
+            public static readonly ResultCode ObjectResult = new ResultCode(Base + 1, CommonMessages.ObjectResult, Group);
+            public static readonly ResultCode NotFound = new ResultCode(Base + 2, CommonMessages.NotFound, Group);
+            public static readonly ResultCode BadRequest = new ResultCode(Base + 3, CommonMessages.BadRequest, Group);
+            public static readonly ResultCode AccessDenied = new ResultCode(Base + 4, CommonMessages.AccessDenied, Group);
+            public static readonly ResultCode UnknownError = new ResultCode(Base + 5, CommonMessages.UnknownError, Group);
+            public static readonly ResultCode PersistenceError = new ResultCode(Base + 6, CommonMessages.PersistenceError, Group);
+            public static readonly ResultCode InvalidPagination = new ResultCode(Base + 7, CommonMessages.InvalidPagination, Group);
+            public static readonly ResultCode InvalidData = new ResultCode(Base + 8, CommonMessages.InvalidData, Group);
         }
     }
 }
