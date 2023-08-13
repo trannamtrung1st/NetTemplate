2f375c5c905f47e5340a5ba98635133ce3b15e70
trannamtrung1st <trannamtrung1st@gmail.com>
2023-08-13 21:07:35 +0700

    Add deep clone

---
+++ Common/NetTemplate.Common/Objects/ObjectUtils.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace NetTemplate.Common.Objects
+{
+    public static class ObjectUtils
+    {
+        public static T JsonDeepClone<T>(this T srcObj)
+        {
+            return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(srcObj));
+        }
+    }
+}
