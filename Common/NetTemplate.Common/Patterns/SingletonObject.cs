ce7d63f224e9b68d3ede21addd0dd50519ddf51b
trannamtrung1st <trannamtrung1st@gmail.com>
2023-08-13 20:45:15 +0700

    Add threadsafe singleton

---
+++ Common/NetTemplate.Common/Patterns/SingletonObject.cs
@@ -0,0 +1,11 @@
+﻿namespace NetTemplate.Common.Patterns
+{
+    public class SingletonObject
+    {
+        private static Lazy<SingletonObject> _instance = new Lazy<SingletonObject>();
+
+        private SingletonObject() { }
+
+        public static SingletonObject Instance => _instance.Value;
+    }
+}
